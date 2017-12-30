a5bd50e94cb1b45a0b675422368c9a9d2409b1b3
Qsior <lukaszbojes@gmail.com>
2017-12-30 01:00:31 +0100

    Implemented basic functions of contact list in mobile phone using ArrayList

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,8 +1,124 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("777777777");
+
     public static void main(String[] args) {
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while (!quit){
+            System.out.println("Enter action: ");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action){
+                case 0:
+                    System.out.println("Shutting down...");
+                    quit = true;
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    queryContact();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+            }
+        }
+    }
+
+    private static void addNewContact(){
+        System.out.println("Enter new contact name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phone = scanner.nextLine();
+        Contact newContact = Contact.addContact(name , phone);
+        if(mobilePhone.addNewContact(newContact)){
+            System.out.println("New contact added: name = " + name + ", phone = " + phone);
+        } else{
+            System.out.println("Cannot add. " + name + " already on file");
+        }
+    }
+
+    private static void updateContact(){
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        Contact existingContact = mobilePhone.queryContact(name);
+        if (existingContact == null){
+            System.out.println("Contact not found");
+            return;
+        }
+        System.out.println("Enter new contact name: ");
+        String newName = scanner.nextLine();
+        System.out.println("Enter new contact phone number: ");
+        String newPhoneNumber = scanner.nextLine();
+        Contact newContact = Contact.addContact(newName,newPhoneNumber);
+        if(mobilePhone.updateContact(existingContact, newContact)){
+            System.out.println("Successfully updated record");
+        } else {
+            System.out.println("Error updating error");
+        }
 
     }
+
+    private static void removeContact(){
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        Contact existingContact = mobilePhone.queryContact(name);
+        if (existingContact == null){
+            System.out.println("Contact not found");
+            return;
+        }
+
+        if(mobilePhone.removeContact(existingContact)){
+            System.out.println("Successfully deleted");
+        } else {
+            System.out.println("Error deleting contact");
+        }
+    }
+
+    private static void queryContact(){
+        System.out.println("Enter existing contact name: ");
+        String name = scanner.nextLine();
+        Contact existingContact = mobilePhone.queryContact(name);
+        if (existingContact == null){
+            System.out.println("Contact not found");
+            return;
+        }
+
+        System.out.println("Name: " + existingContact.getName() + ", Phone: " + existingContact.getPhoneNumber());
+    }
+
+    private static void startPhone(){
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions(){
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println("0 - to shutdown\n" +
+                "1 - to print contacts\n" +
+                "2 - to add a new contact\n" +
+                "3 - to update an existing contact\n" +
+                "4 - to remove an existing contact\n" +
+                "5 - query if an existing contact exists\n" +
+                "6 - to print a lost of available actions\n" );
+        System.out.println("Choose your action: ");
+    }
+
 }
